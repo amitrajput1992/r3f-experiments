ecc95dcc220cd4f426cf324bb76589b6c6254012
Amit Rajput <amitrajput123.ar@gmail.com>
2022-02-09 15:01:39 +0530

    changing file address

--- stories/GifTalkr2.stories.tsx
+++ stories/GifTalkr2.stories.tsx
@@ -7,7 +7,7 @@ export default {
 
 const GifTalkrB = () => {
   return (
-    <GifTalkrC gifURL={"https://s.vrgmetri.com/gm-gb-test/minc/safehands_transparent_cut.gif"}/>
+    <GifTalkrC gifURL={"https://u.vrgmetri.com/gm-gb-test/minc/safehands_transparent_cut.gif"}/>
   );
 }
 
